30f12881f2e99432fa7be82cd5f5e9f49a153cd7
Daniil Lebedzeu <danle@sam-solutions.net>
2019-12-10 12:07:19 +0300

    fix

---
+++ src/ts/getData.ts
@@ -0,0 +1,10 @@
+// class Data {
+//     url: string
+//     private async getData(): Promise<T> {
+//       const response = await fetch(this.url);
+//       const result = await response.json()
+//       const rockEvents = result[0].rock[0].events[0]
+//       console.log(rockEvents)
+//     }
+//
+// }
--- src/ts/index.js
+++ src/ts/index.js
@@ -1,2 +1,27 @@
-'use strict';
-console.log('hello');
+// async function getData(url) {
+//     // var data;
+//     // fetch(url)
+//     //     .then(response => response.json())
+//     //     .then(categories => console.log(data = categories[0].rock[0].events));
+//     let response = await fetch(url);
+//     let result = await response.json()
+//     let rockEvents = result[0].rock[0].events[0]
+//     console.log(rockEvents)
+//     renderEvent(rockEvents)
+//
+// }
+//
+// function renderEvent(event) {
+//     let body = document.querySelector('body')
+//     for (let key in event) {
+//         if (key === 'img') {
+//             body.innerHTML += `<img src=${event[key]}>`
+//             break
+//         }
+//         body.innerHTML += `<li>${key}: ${event[key]}</li>`
+//     }
+// }
+//
+//
+//
+// getData('http://localhost:3000/music')
