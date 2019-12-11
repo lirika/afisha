e13e246a81c9f72ff932cb1bd50ac6b09dd14adf
Aliaksandr Sluka <alesl@sam-solutions.net>
2019-12-11 11:32:40 +0300

    add navbar

---
+++ src/js/navbar.js
@@ -0,0 +1,18 @@
+'use strict';
+function navbar() {
+  let selected;
+  document.getElementById('navbar').addEventListener('click', ev => {
+    let target_item = ev.target;
+    if (target_item.className != 'item') return;
+    active(target_item);
+  });
+
+  function active(item) {
+    if (selected) {
+      selected.classList.remove('active');
+    }
+    selected = item;
+    selected.classList.add('active');
+  }
+}
+navbar();
