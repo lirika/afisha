975954fde6f9fa1317da1bbdce7bcb5db2b1a8ee
Yana Karcheuskaya <yana-03032017@mail.ru>
2019-12-13 10:11:11 +0300

    items fixed: added events for buttons and horizontal scroll element

--- src/ts/events.js
+++ src/ts/events.js
@@ -4,40 +4,66 @@ var dark = document.getElementById('dark');
 var navbar = document.getElementById('navbar');
 var burgerActive = false;
 function setNavbar() {
-  var selected;
-  navbar.addEventListener('click', function(ev) {
-    var targetItem = ev.target;
-    // @ts-ignore
-    if (targetItem.className !== 'item') {
-      return;
+    var selected;
+    navbar.addEventListener('click', function (ev) {
+        var targetItem = ev.target;
+        // @ts-ignore
+        if (targetItem.className !== 'item') {
+            return;
+        }
+        active(targetItem);
+    });
+    function active(item) {
+        if (selected) {
+            selected.classList.remove('active');
+        }
+        selected = item;
+        selected.classList.add('active');
     }
-    active(targetItem);
-  });
-  function active(item) {
-    if (selected) {
-      selected.classList.remove('active');
-    }
-    selected = item;
-    selected.classList.add('active');
-  }
 }
 setNavbar();
 function burgerHide() {
-  burger.classList.remove('burger-active');
-  header.classList.remove('header-animation');
-  burgerActive = false;
-  dark.style.display = 'none';
+    burger.classList.remove('burger-active');
+    header.classList.remove('header-animation');
+    burgerActive = false;
+    dark.style.display = 'none';
 }
-burger.addEventListener('click', function() {
-  if (burgerActive === false) {
-    burger.classList.add('burger-active');
-    burgerActive = true;
-    header.classList.add('header-animation');
-    dark.style.display = 'block';
-  } else {
-    burgerHide();
-  }
+burger.addEventListener('click', function () {
+    if (burgerActive === false) {
+        burger.classList.add('burger-active');
+        burgerActive = true;
+        header.classList.add('header-animation');
+        dark.style.display = 'block';
+    }
+    else {
+        burgerHide();
+    }
 });
-dark.addEventListener('click', function() {
-  burgerHide();
+dark.addEventListener('click', function () {
+    burgerHide();
+});
+//events items
+var count = 4;
+var wrap = document.querySelector('.wrap-items');
+var items = document.querySelector('.items');
+var ul = document.querySelector('.wrap-items ul');
+var listLi = document.querySelectorAll('.events li');
+var prev = document.querySelector('.prev');
+var next = document.querySelector('.next');
+var scrollThumb = document.querySelector('.scroll-thumb');
+listLi.forEach(function (li) {
+    li.addEventListener('click', function (event) {
+        /*  alert(li.offsetTop);
+          wrap.scrollBy(0, li.offsetTop); */
+        scrollThumb.style.marginLeft = (li.offsetLeft - ul.offsetLeft) + "px";
+    });
+});
+var position = 0;
+prev.addEventListener('click', function (event) {
+    position += 190;
+    ul.style.marginTop = position + "px";
+});
+next.addEventListener('click', function (event) {
+    position -= 190;
+    ul.style.marginTop = position + "px";
 });
--- src/ts/events.ts
+++ src/ts/events.ts
@@ -48,3 +48,39 @@ burger.addEventListener('click', () => {
 dark.addEventListener('click', () => {
   burgerHide();
 });
+
+//events items
+
+
+const count :number = 4;
+
+const wrap = document.querySelector('.wrap-items') as HTMLDivElement;
+const items = document.querySelector('.items') as HTMLDivElement;
+const ul = document.querySelector('.wrap-items ul') as HTMLUListElement;
+const listLi = document.querySelectorAll<HTMLElement>('.events li');
+const prev = document.querySelector('.prev') as HTMLButtonElement;
+const next = document.querySelector('.next') as HTMLButtonElement;
+
+const scrollThumb = document.querySelector('.scroll-thumb') as HTMLDivElement;
+
+listLi.forEach(li => {
+    li.addEventListener('click', (event) => {
+    /*  alert(li.offsetTop);
+      wrap.scrollBy(0, li.offsetTop); */
+
+      scrollThumb.style.marginLeft = (li.offsetLeft - ul.offsetLeft) + "px";
+    });
+});
+
+let position :number = 0;
+
+prev.addEventListener('click', (event) => {
+    position += 190;
+    ul.style.marginTop = position + "px"; 
+});
+
+next.addEventListener('click', (event) => {
+    position -= 190;
+    ul.style.marginTop = position + "px"; 
+});
+
